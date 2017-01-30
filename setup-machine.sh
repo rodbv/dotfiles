c0402c3f00d937de12e8961a636c140d14c45696
Adroaldo de Andrade <adroaldof@gmail.com>
2017-01-30 08:13:30 -0200

    Add VSCode to installs

--- setup-machine.sh
+++ setup-machine.sh
@@ -59,6 +59,32 @@ sudo apt-get install silversearcher-ag -qqy
 
 
 ###############################################################################
+# Install VSCode
+###############################################################################
+curl -L https://go.microsoft.com/fwlink/?LinkID=760868 > /tmp/vscode.deb
+sudo dpkg -i /tmp/vscode.deb && sudo apt-get install -f
+
+# Install some VSCode Extensions
+code --install-extension aeschli.vscode-css-formatter
+code --install-extension alefragnani.Bookmarks
+code --install-extension christian-kohler.path-intellisense
+code --install-extension cssho.vscode-svgviewer
+code --install-extension donjayamanne.githistory
+code --install-extension EditorConfig.EditorConfig
+code --install-extension eg2.tslint
+code --install-extension MattiasPernhult.vscode-todo
+code --install-extension ms-vscode.csharp
+code --install-extension ms-vscode.PowerShell
+code --install-extension PeterJausovec.vscode-docker
+code --install-extension QassimFarid.ejs-language-support
+code --install-extension seanmcbreen.Spell
+code --install-extension Shan.code-settings-sync
+code --install-extension stevencl.addDocComments
+code --install-extension wmaurer.change-case
+code --install-extension zhutian.swig
+
+
+###############################################################################
 # Install xClip
 ###############################################################################
 echo "Installing xClip"
